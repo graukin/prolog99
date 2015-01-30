e4856d7f95e2afa82001bc530822afb3ec5b4279
Takesha <graukin@gmail.com>
2015-01-31 01:15:15 +0300

    add first group and solve first problem (with tests).

---
+++ 01_prolog_lists/pr01.pl
@@ -0,0 +1,13 @@
+% 1.01 (*) Find the last element of a list.
+%    Example:
+%    ?- my_last(X,[a,b,c,d]).
+%    X = d
+
+l_elem([], []).
+l_elem(X, [X]).
+l_elem(X, [_|T]) :- l_elem(X,T).
+
+test1 :- l_elem(A,[]), A = [].
+test2 :- l_elem(B,[a]), B = a.
+test3 :- l_elem(C,[a,b,c,d]), C = d.
+test4 :- l_elem(D,[a,b,[c,d]]), D = [c,d].
